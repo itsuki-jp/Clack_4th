be5c0601ca158dbe044bcad212f97e1a2848fb1d
n_itsuki <itsuki19601@gmail.com>
2023-02-06 22:53:24 +0900

    誤読供養

--- The_Yacht_Game/index.js
+++ The_Yacht_Game/index.js
@@ -3,3 +3,34 @@ const main = document.getElementById("main");
 const tableArea = document.createElement("div");
 const diceArea = document.createElement("div");
 
+function countNum(num, arr) {
+  let res = 0; // numがarrに何個あるか
+  let continuous = 0; // numが最大何個連続してるか
+  let continuousMax = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] !== num) {
+      continuousMax = continuousMax > continuous ? continuousMax : continuous;
+      continuous = 0;
+    } else {
+      res += 1;
+      continuous++;
+    }
+  }
+  continuousMax = continuousMax > continuous ? continuousMax : continuous;
+  return [res, continuousMax];
+}
+const nameAndScore = [
+  { name: "Aces", param: 1 },
+  { name: "Twos", param: 2 },
+  { name: "Threes", param: 3 },
+  { name: "Fours", param: 4 },
+  { name: "Fives", param: 5 },
+  { name: "Sixes", param: 6 },
+  { name: "3 of Kind" },
+  { name: "4 of Kind" },
+  { name: "Small Straight" },
+  { name: "Large Straight" },
+  { name: "YAHTZEE" },
+  { name: "Chance" },
+  
+];
